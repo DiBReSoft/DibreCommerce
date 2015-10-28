ea5ac33aea7a41747bfd76a54cc5a5607c45e427
luciano <luciano@KANSAS-TI2>
2015-10-28 17:23:54 -0200

    Método de incluir, alterar e inativar funcionando!!
    O problema da data foi corrigido de forma temporaria. Estou estudando uma solução melhor.

--- src/main/java/br/com/dibrecommerce/model/cliente/ClienteDAO.java
+++ src/main/java/br/com/dibrecommerce/model/cliente/ClienteDAO.java
@@ -6,10 +6,8 @@
 package br.com.dibrecommerce.model.cliente;
 
 import br.com.dibrecommerce.persistencia.ConnectionFactory;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -19,29 +17,30 @@ import java.util.logging.Logger;
  */
 public class ClienteDAO {
 
-  public boolean cadastrarCliente(Cliente cli) {
+  public boolean cadastrarCliente(Cliente c) {
+    System.out.println("data que vem do browser"+c.getDt_nasc());
+    
     ConnectionFactory conexao = new ConnectionFactory();
     PreparedStatement stmt = null;
+    c.setStatus(true); 
     String sql = "INSERT INTO APP.TB_CLIENTE (NM_CLIENTE, SEXO_CLIENTE, DT_NASC_CLIENTE, CPF_CLIENTE, RG_CLIENTE, EMAIL_CLIENTE, SENHA_CLIENTE, NEWSLETTER, PRIVACIDADE, STATUS, DT_CADASTRO) \n"
 	    + "	VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP)";
 
     try {
       conexao.openConection();
       stmt = conexao.conn.prepareStatement(sql);
-      
-      cli.setStatus(true);
-
-      stmt.setString(1, cli.getNome());
-      stmt.setString(2, cli.getSexo());
-      java.sql.Date sqlDataNasc = new java.sql.Date(cli.getDt_nasc().getTime());
-      stmt.setDate(3, sqlDataNasc);
-      stmt.setString(4, cli.getCpf());
-      stmt.setString(5, cli.getRg());
-      stmt.setString(6, cli.getEmail());
-      stmt.setString(7, cli.getSenha());
-      stmt.setBoolean(8, cli.isNewsletter());
-      stmt.setBoolean(9, cli.isPrivacidade());
-      stmt.setBoolean(10, cli.isStatus());
+      stmt.setString(1, c.getNome());
+      stmt.setString(2, c.getSexo());
+      //O número 8440000000 é equivalente a um dia. Isso corrige temporariamente a falha na inserção de datas no sistema. Ass.Luciano
+      java.sql.Date sqlDate = new java.sql.Date(c.getDt_nasc().getTime()+84400000);
+      stmt.setDate(3,  sqlDate);
+      stmt.setString(4, c.getCpf());
+      stmt.setString(5, c.getRg());
+      stmt.setString(6, c.getEmail());
+      stmt.setString(7, c.getSenha());
+      stmt.setBoolean(8, c.isNewsletter());
+      stmt.setBoolean(9, c.isPrivacidade());
+      stmt.setBoolean(10, c.isStatus());
 
       stmt.executeUpdate();
 
@@ -103,54 +102,53 @@ public class ClienteDAO {
     return true;
   }
   
-//  public boolean editarCliente(Cliente c, int id) {
-//
-//        ConnectionFactory conexao = new ConnectionFactory();
-//        PreparedStatement stmt = null;
-//
-//        String sql = "UPDATE TB_CLIENTE SET NM_CLIENTE = ?, SEXO_CLIENTE = ?, DT_NASC_CLIENTE = ?, "
-//                + "SENHA_CLIENTE=?, NEWSLETTER = ? \n"
-//                + "WHERE ID_CLIENTE = ? \n";
-//
-//        try {
-//            conexao.openConection();
-//            stmt = conexao.conn.prepareStatement(sql);
-//
-//            stmt.setString(1, c.getNome());
-//            stmt.setString(2, c.getSexo());
-//            java.sql.Date sqlDataNasc;
-//	    SimpleDateFormat sdf = new SimpleDateFormat("28/11/1988");
-//	    sqlDataNasc = sdf.parse('1988-11-28');
-//            stmt.setDate(3, sqlDataNasc);
-//	    stmt.setString(4,c.getSenha());
-//            stmt.setBoolean(5, c.isNewsletter());
-//	    stmt.setInt(6,id);
-//            
-//
-//            stmt.executeUpdate();
-//            System.out.println("Dados Alterados com sucesso!!!");
-//
-//        } catch (SQLException ex) {
-//            Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
-//            System.out.println("Erro ao alterar os dados");
-//            return false;
-//        } finally {
-//            if (stmt != null) {
-//                try {
-//                    stmt.close();
-//                } catch (SQLException ex) {
-//                    Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
-//                }
-//            }
-//            if (conexao.conn != null) {
-//                try {
-//                    conexao.conn.close();
-//                } catch (SQLException ex) {
-//                    Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
-//                }
-//            }
-//        }
-//        return true;
-//    }
+  public boolean editarCliente(Cliente c, int id) {
+
+        ConnectionFactory conexao = new ConnectionFactory();
+        PreparedStatement stmt = null;
+
+        String sql = "UPDATE TB_CLIENTE SET NM_CLIENTE = ?, SEXO_CLIENTE = ?, DT_NASC_CLIENTE = ?, "
+                + "SENHA_CLIENTE=?, NEWSLETTER = ? \n"
+                + "WHERE ID_CLIENTE = ? \n";
+
+        try {
+            conexao.openConection();
+            stmt = conexao.conn.prepareStatement(sql);
+
+            stmt.setString(1, c.getNome());
+            stmt.setString(2, c.getSexo());
+            //O número 8440000000 é equivalente a um dia. Isso corrige temporariamente a falha na inserção de datas no sistema.
+            java.sql.Date sqlDate = new java.sql.Date(c.getDt_nasc().getTime()+84400000);
+            stmt.setDate(3, sqlDate);
+	    stmt.setString(4,c.getSenha());
+            stmt.setBoolean(5, c.isNewsletter());
+	    stmt.setInt(6,id);
+            
+
+            stmt.executeUpdate();
+            System.out.println("Dados Alterados com sucesso!!!");
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Erro ao alterar os dados");
+            return false;
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (conexao.conn != null) {
+                try {
+                    conexao.conn.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        return true;
+    }
 
 }
